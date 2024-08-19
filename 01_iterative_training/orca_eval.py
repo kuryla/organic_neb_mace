f7e5727c56aa789a21530d098435ed4586f5eb40
Domantas Kuryla <dk584@cam.ac.uk>
2024-08-19 18:08:06 +0300

    orca_eval

--- 01_iterative_training/orca_eval.py
+++ 01_iterative_training/orca_eval.py
@@ -2,7 +2,6 @@ from ase.calculators.orca import ORCA, OrcaProfile
 import argparse
 from ase.io import read, write
 import os
-from ase.optimize import LBFGS
 from ase import Atoms
 
 def parse_args() -> argparse.Namespace:
@@ -12,6 +11,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--output", help="path to save output configs to", required=True)
     parser.add_argument("--charge", help="charge", default=0, type=int)
     parser.add_argument("--mult", help="spin multiplicity", default=1, type=int)
+    parser.add_argument("--orca_path", help="path to orca installation", required=True)
     return parser.parse_args()
 
 def get_calc(orca_path, charge=0, mult=1):
@@ -24,7 +24,7 @@ def get_calc(orca_path, charge=0, mult=1):
     )
     return calc
 
-def single_point(orca_path, config, charge, mult, prefix="mp2_"):
+def single_point(config, orca_path, charge, mult, prefix="mp2_"):
     calc = get_calc(orca_path, charge, mult)
     calc.calculate(atoms=config, properties={"energy", "forces"}, system_changes=None)
     os.system("rm orca_property.txt orca.*")
@@ -44,15 +44,16 @@ def single_point(orca_path, config, charge, mult, prefix="mp2_"):
 def main():
     args = parse_args()
     configs = read(args.input, ":")
+    orca_path = args.orca_path
 
     if isinstance(configs, list):
         for config in configs:
-            config = single_point(config, args.charge, args.mult)
+            config = single_point(config, orca_path, args.charge, args.mult)
         write(args.output, configs)
         os.system("rm orca.* orca_property.txt")
 
     elif isinstance(configs, Atoms):
-        configs = single_point(configs, args.charge, args.mult)
+        configs = single_point(configs, orca_path, args.charge, args.mult)
         write(args.output, configs)
         os.system("rm orca.* orca_property.txt")
 
